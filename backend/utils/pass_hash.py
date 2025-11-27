e2eb79cc12f9448115cdc236c627f48a28d830b0
Umaan Banjara <umaanbanjara@gmail.com>
2025-11-27 13:11:01 +0545

    util function for hashing password

---
+++ backend/utils/pass_hash.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=['bcrypt']) #context schema
+
+
+def hash_pass(plain_pass : str) -> str:
+    hashedPassword = pwd_context.hash(plain_pass)
+    return hashedPassword
+
+def verify_pass (plain_pass : str , hashed_pass : str) -> bool:
+    verifiedPassword = pwd_context.verify(plain_pass , hashed_pass)
+    return verifiedPassword
+
