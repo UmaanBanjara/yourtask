f47e0976a084ae92bd937737e517cd8a429b44c7
Umaan Banjara <umaanbanjara@gmail.com>
2025-11-27 13:06:52 +0545

    main router file

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.routes.signup import router as signup_router
+
+
+app = FastAPI(title='YourTask')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+app.include_router(signup_router , prefix='/auth' , tags = ['Authorization'])
+
+#root endpoint
+@app.get('/')
+async def root():
+    return {
+        'message' : 'Welcome to YourTask'
+    }
