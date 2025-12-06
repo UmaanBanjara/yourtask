4751cee9308e8048359ee00112f8312d595b89c4
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-06 12:34:07 +0545

    added rate limit (2 requests per hour)

--- backend/routes/resendotp.py
+++ backend/routes/resendotp.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter , HTTPException
+from fastapi import APIRouter , HTTPException, Request
 from backend.CRUD.resendotp import resend_otp
 from pydantic import BaseModel , EmailStr
+from backend.utils.rate_limiter import rate_limited
 
 
 class Usercheck(BaseModel):
@@ -10,7 +11,15 @@ class Usercheck(BaseModel):
 router = APIRouter()
 
 @router.post('/resend/otp')
-async def otp_resend(otp : Usercheck):
+async def otp_resend(otp : Usercheck , request : Request):
+    user_ip = request.client.host
+    allowed = await rate_limited(
+        key = f"resentotp:{user_ip}",
+        limit=2,
+        time_window=60*60
+    )
+    if not allowed:
+        raise HTTPException(status_code=401 , detail="Too many attemps. Please try again later sometime")
     otpresend = await resend_otp(
         email=otp.email,
         userId=otp.userId
