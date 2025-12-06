c08cf107b06adcd630cbe3d967282431e44bc358
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-06 12:33:17 +0545

    added rate limit (3 request per hour)

--- backend/routes/login.py
+++ backend/routes/login.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter , HTTPException
+from fastapi import APIRouter , HTTPException , Request
 from backend.CRUD.login import verify_user
 from pydantic import BaseModel , EmailStr
+from backend.utils.rate_limiter import rate_limited
 
 class Usercheck(BaseModel):
     email : EmailStr
@@ -9,7 +10,15 @@ class Usercheck(BaseModel):
 router = APIRouter()
 
 @router.post('/login')
-async def login(user : Usercheck):
+async def login(user : Usercheck , request : Request):
+        user_ip = request.client.host
+        allowed = await rate_limited(
+             key=f"login:{user_ip}",
+             limit=3,
+             time_window=60*60
+        )
+        if not allowed:
+             raise HTTPException(status_code=401 , detail="Too many login attemps. Please try again later sometime")
         verify = await verify_user(email=user.email , password=user.password)
         if 'access_token' not in verify:
             raise HTTPException(status_code=400 , detail=verify['message'])
