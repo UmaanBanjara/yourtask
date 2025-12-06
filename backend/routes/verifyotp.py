cfcad4e7738ed4c553f46cd8186585e59c7344f6
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-06 12:35:01 +0545

    added rate limit(2 requests per hour)

--- backend/routes/verifyotp.py
+++ backend/routes/verifyotp.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter , HTTPException
+from fastapi import APIRouter , HTTPException, Request
 from backend.CRUD.verifyotp import verify_otp
 from pydantic import BaseModel
+from backend.utils.rate_limiter import rate_limited
 
 
 class UserCheck(BaseModel):
@@ -11,7 +12,15 @@ router  = APIRouter()
 
 @router.post('/otp/verify')
 
-async def otpverify(otpcheck : UserCheck):
+async def otpverify(otpcheck : UserCheck , request : Request):
+    user_ip = request.client.host
+    allowed = await rate_limited(
+        key=f"verifyotp:{user_ip}",
+        limit=2,
+        time_window=60*60
+    )
+    if not allowed:
+        raise HTTPException(status_code=401 , detail="Too many attemps. Please try again later sometime")
     otp_verify = await verify_otp(
         otpcheck.code,
         otpcheck.userId
