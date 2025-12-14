215a8778d6606618f32cd4097c6461f69307c969
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-14 14:11:34 +0545

    added rate limit

--- backend/routes/login.py
+++ backend/routes/login.py
@@ -12,13 +12,13 @@ router = APIRouter()
 @router.post('/login')
 async def login(user: Usercheck, request: Request):
     user_ip = request.client.host
-    # allowed = await rate_limited(
-    #     key=f"login:{user_ip}",
-    #     limit=3,
-    #     time_window=60*60
-    # )
-    # if not allowed:
-    #     raise HTTPException(status_code=401, detail="Too many login attempts. Please try again later sometime")
+    allowed = await rate_limited(
+        key=f"login:{user_ip}",
+        limit=3,
+        time_window=60*60
+    )
+    if not allowed:
+        raise HTTPException(status_code=401, detail="Too many login attempts. Please try again later sometime")
     
     verify = await verify_user(email=user.email, password=user.password)
     if 'access_token' not in verify:
