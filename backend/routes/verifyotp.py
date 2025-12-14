22fc5ce57796d462efd119abaef7959dd13ef7c2
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-14 12:09:13 +0545

    verifyotp with rate limit

--- backend/routes/verifyotp.py
+++ backend/routes/verifyotp.py
@@ -1,18 +1,16 @@
-from fastapi import APIRouter , HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request
 from backend.CRUD.verifyotp import verify_otp
 from pydantic import BaseModel
 from backend.utils.rate_limiter import rate_limited
 
-
 class UserCheck(BaseModel):
-    userId : int
-    code : str
+    userId: int
+    code: str
 
-router  = APIRouter()
+router = APIRouter()
 
 @router.post('/otp/verify')
-
-async def otpverify(otpcheck : UserCheck , request : Request):
+async def otpverify(otpcheck: UserCheck, request: Request):
     user_ip = request.client.host
     allowed = await rate_limited(
         key=f"verifyotp:{user_ip}",
@@ -20,7 +18,8 @@ async def otpverify(otpcheck : UserCheck , request : Request):
         time_window=60*60
     )
     if not allowed:
-        raise HTTPException(status_code=401 , detail="Too many attemps. Please try again later sometime")
+        raise HTTPException(status_code=401, detail="Too many attempts. Please try again later sometime")
+    
     otp_verify = await verify_otp(
         otpcheck.code,
         otpcheck.userId
@@ -28,8 +27,9 @@ async def otpverify(otpcheck : UserCheck , request : Request):
 
     if otp_verify['message'] != 'OTP verified successfully':
         return {
-            'message' : otp_verify['message']
+            'message': otp_verify['message']
         }
+    
     return {
-        'message' : otp_verify['message']
+        'message': otp_verify['message']
     }
