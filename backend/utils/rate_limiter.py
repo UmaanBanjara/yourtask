6001c1d98156e36edd02f2edeb1ef656206efa2f
Umaan Banjara <umaanbanjara@gmail.com>
2025-12-14 12:09:34 +0545

    utility rate limit function

--- backend/utils/rate_limiter.py
+++ backend/utils/rate_limiter.py
@@ -8,7 +8,11 @@ async def rate_limited(key : str , limit : int , time_window : int) -> bool:
     current =  await redis_client.incr(key)
 
     if current==1:
-        await redis_client.set(key , 1 , ex=time_window)
+        await redis_client.expire(key , time_window)
+        print(f"Key is : {key} created with expiration  : {time_window}")
+    
+    ttl = await redis_client.ttl(key)
+    print(f"Time left for the key is {ttl}")
     
     if current <= limit:
         return True
